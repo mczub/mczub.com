7df7ba7d663c20a4d4d633a744582e2acf8e4868
mczub <mczub@outlook.com>
2014-07-10 02:43:13 -0700

    debug

--- set/app.js
+++ set/app.js
@@ -35,6 +35,7 @@ app.get('/users', users.list);
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
+    alert(connectStr);
     err.status = 404;
     next(err);
 });
