3deaca76c9bb4e8b29b8d83e9556d7bad48b34b2
mczub <mczub@outlook.com>
2014-07-10 14:04:59 -0700

    again

--- set/app.js
+++ set/app.js
@@ -82,7 +82,7 @@ app.get('/set', function(req, res){
   res.send('Hello World');
 });
 
-var port = process.env.port || 1337;
+var port = process.env.PORT || 1337;
 app.listen(port,function(){
     console.log('listening on port' + port);
 });
