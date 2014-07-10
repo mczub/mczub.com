7a917df6341606a2f23540cb476f8797b37ccd40
mczub <mczub@outlook.com>
2014-07-10 14:03:30 -0700

    playing with app.js and config again

--- set/app.js
+++ set/app.js
@@ -76,7 +76,7 @@ module.exports = app;
 */
 
 var express = require('express');
-var app = express();
+var app = express.createServer();
 
 app.get('/', function(req, res){
   res.send('Hello World');
